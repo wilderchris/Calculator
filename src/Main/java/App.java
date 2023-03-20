41839aa8175371d00dc7c5afaade77a1b4869a37
wilderchris <94183821+wilderchris@users.noreply.github.com>
2023-03-20 14:58:12 -0500

    updated
    added + - * / divide functions with divide by zero check

--- src/Main/java/App.java
+++ src/Main/java/App.java
@@ -4,26 +4,38 @@ public class App {
 
 	public static void main(String[] args) {
 
-
+		args[0] = "42";
+		args[1] = "/";
+		args[2] = "2";
+		System.out.println("CommandLine Calc app : \n Calculator [num] [+ - * /] [num]");
+ 
 		if (args.length == 0) {
 			System.out.println("Place holder for UI app");
-		}else {
-			System.out.println("CommandLine Calc app : \n Calculator [num] [+ - * /] [num]");
-			System.out.println("Response :  " + args[0] +
-					" "+ args[1] + " " + 
-					args[2] + " = " + Calculate(Integer.parseInt(args[0]),args[1], Integer.parseInt(args[2]))) ;
+		} else {
+			System.out.println("Response :\n\n  " + args[0] + " " + args[1] + " " + args[2] + " = "
+					+ Calculate(Integer.parseInt(args[0]), args[1], Integer.parseInt(args[2])));
 		}
-			
-		
-		
+
 	}
-	public static int Calculate(int num1, String symbol, int num2) {
-		
-		if ( symbol.contains("+")) {
-			return num1 + num2;
+
+	public static String Calculate(int num1, String symbol, int num2) {
+
+		if (symbol.contains("+")) {
+			return String.valueOf((num1) + num2);
+		} else if (symbol.contains("-")) {
+			return String.valueOf(num1 - num2);
+		} else if (symbol.contains("*")) {
+			return String.valueOf(num1 * num2);
+		} else if (symbol.contains("/")) {
+			if ((num1 != 0 && num2 != 0)) {
+				return String.valueOf(num1 / num2);
+			} else {
+				System.out.println(" cannot divide by 0");
+				return "Error";
+			}
+
 		}
-		
-		return 0;
-		
+		return "Error";
+
 	}
 }
